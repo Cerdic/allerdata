372b31ba2bd937aff794c339f33d3d80c43495a9
gilles <gilles>
2007-12-06 02:33:18 +0000

    grosse correction des suggestions. Elles fonctionnent mieux, fournissent les lments les plus reactifs d'abord. Par contre, il reste une ambiguite sur les composes - exemple du bouleau et du kiwi : une RC devient incertaine car le 'Bet v 1' ne reagit jamais au kiwi
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@47 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- liste_des_suggestions_fonctions.php
+++ liste_des_suggestions_fonctions.php
@@ -1,6 +1,37 @@
 <?php
+
+function msort($array, $id="id", $sort_ascending=true)
+/* http://fr3.php.net/manual/en/function.sort.php#76547 */
+/* Fonction de tri sur un clef */
+{
+    $temp_array = array();
+    while (count($array)>0) {
+        $lowest_id = 0;
+        $index=0;
+        foreach($array as $item) {
+            if (isset($item[$id])) {
+                if ($array[$lowest_id][$id]) {
+                    if ($item[$id]<$array[$lowest_id][$id]) {
+                        $lowest_id = $index;
+                    }
+                }
+            }
+            $index++;
+        }
+        $temp_array[] = $array[$lowest_id];
+        $array = array_merge(array_slice($array, 0,$lowest_id), array_slice($array, $lowest_id+1));
+    }
+    if ($sort_ascending) {
+        return $temp_array;
+    } else {
+        return array_reverse($temp_array);
+    }
+}
+
+
 function suggestions($txt) {
 	$tableau_produits = array();
+	$prem = array(17,97); /* 2 nombres premiers avant 100 avec un delta de 1/8 */
 	
 	if (is_numeric($_REQUEST['p1'])) $tableau_produits[] = $_REQUEST['p1']; 
 	if (is_numeric($_REQUEST['p2'])) $tableau_produits[] = $_REQUEST['p2'];
@@ -15,7 +46,7 @@ function suggestions($txt) {
 	$tt = '';
 	
 	/* Sens Aller : p1 vers p2 */
-
+	/* Les produits connus sont les produits source */
 	$query = "
 	SELECT tbl_items.id_item AS idp1, 
 		tbl_items.nom AS p1, 
@@ -31,7 +62,8 @@ function suggestions($txt) {
 		AND tbl_items.id_item
 			IN ($produits ) 
 		AND tbl_items_1.id_item NOT 
-			IN ($produits) ".
+			IN ($produits)
+			AND tbl_items_1.interrogeable = 1 ". 
 	//	AND tbl_items_1.affichage_suggestion =1
 	"GROUP BY tbl_reactions_croisees.id_reaction_croisee
 	ORDER BY tbl_items.nom
@@ -40,42 +72,47 @@ function suggestions($txt) {
 	$res = spip_query($query);
 	$liste = $suggestions = $nom = array();
 		
-	spip_log($query);
-	
+	/* On stocke de façon à avoir "en premier" les produits suggérés */
+	/* D'où la permutation -- qui ne sert qu'a l'affichage */
 	while ($row = spip_fetch_array($res)){
 			
-		$nom[$row['idp1']] = $row['p1'];
 		$nom[$row['idp2']] = $row['p2'];
-		
+		$nom[$row['idp1']] = $row['p1'];
+
 		/* 3 cas possibles : NULL, 0 ou 1  */
-		if (!isset($liste[$row['idp1']])) $liste[$row['idp1']] = array();
-		if (!isset($liste[$row['idp1']][$row['idp2']])) 
-			$liste[$row['idp1']][$row['idp2']] = array();
-		if (!isset($liste[$row['idp1']][$row['idp2']])) {
-			$s1 = $s2 = null;
-			if (!is_null($row['fleche_sens1'])) $s1 = $row['fleche_sens1'];
-			if (!is_null($row['fleche_sens2'])) $s2 = $row['fleche_sens2'];
-			$liste[$res['idp1']][$row['idp2']] = array('s1' => $s1,'s2' => $s2);
+		/* Astuce pour retrouver les discordants: 
+		  utiliser les nombres premiers */
+
+		if (!isset($liste[$row['idp2']])) $liste[$row['idp2']] = array();
+		if (!isset($liste[$row['idp2']][$row['idp1']])) 
+			$liste[$row['idp2']][$row['idp1']] = array();
+		if (!isset($liste[$row['idp2']][$row['idp1']])) {
+			$s1 = $s2 = 0;
+			if (!is_null($row['fleche_sens1'])) $s2 = $prem[$row['fleche_sens1']];
+			if (!is_null($row['fleche_sens2'])) $s1 = $prem[$row['fleche_sens2']];
+			$liste[$row['idp2']][$row['idp1']] = array('s1' => $s1,'s2' => $s2);
 		}
 		else {
-			$s1 = $liste[$row['idp1']][$row['idp2']]['s1'];
-			$s2 = $liste[$row['idp1']][$row['idp2']]['s2'];
-			if (!is_null($row['fleche_sens1'])) $s1 += $row['fleche_sens1'];
-			if (!is_null($row['fleche_sens2'])) $s2 += $row['fleche_sens2'];
-			$liste[$row['idp1']][$row['idp2']] = array('s1' => $s1,'s2' => $s2);
+			$s1 = $liste[$row['idp2']][$row['idp1']]['s1'];
+			$s2 = $liste[$row['idp2']][$row['idp1']]['s2'];
+			if (!is_null($row['fleche_sens1'])) $s2 += $prem[$row['fleche_sens1']];
+			if (!is_null($row['fleche_sens2'])) $s1 += $prem[$row['fleche_sens2']];
+			$liste[$row['idp2']][$row['idp1']] = array('s1' => $s1,'s2' => $s2);
 		}
 		
 	}
 	
 	/* Sens Retour : p2 vers p1 */
+	/* On permute les extrémités des RC pour la recherche,
+		en indiquant que ce sont les produits cibles qui sont concernés */
 
 	$query = "
-	SELECT tbl_items_1.id_item AS idp1, 
-		tbl_items_1.nom AS p1, 
-		tbl_reactions_croisees.fleche_sens1, 
-		tbl_reactions_croisees.fleche_sens2, 
-		tbl_items.id_item AS idp2, 
-		tbl_items.nom AS p2
+		SELECT tbl_items.id_item AS idp1, 
+			tbl_items.nom AS p1, 
+			tbl_reactions_croisees.fleche_sens1, 
+			tbl_reactions_croisees.fleche_sens2, 
+			tbl_items_1.id_item AS idp2, 
+			tbl_items_1.nom AS p2
 	FROM tbl_est_dans, tbl_est_dans AS tbl_est_dans_1, tbl_reactions_croisees, tbl_items, tbl_items AS tbl_items_1
 	WHERE tbl_est_dans.id_item = tbl_reactions_croisees.id_produit1
 		AND tbl_est_dans_1.id_item = tbl_reactions_croisees.id_produit2
@@ -84,14 +121,14 @@ function suggestions($txt) {
 		AND tbl_items_1.id_item
 			IN ($produits ) 
 		AND tbl_items.id_item NOT 
-			IN ($produits) ".
+			IN ($produits)
+			AND tbl_items.interrogeable = 1 ". 
 	//	AND tbl_items.affichage_suggestion =1
 	"GROUP BY tbl_reactions_croisees.id_reaction_croisee
 	ORDER BY tbl_items_1.nom
 	";
 		
-	spip_log($query);
-	
+	/* Par contre il faut le stocker dans le tableau dans l'ordre inverse */
 	$res = spip_query($query);
 	while ($row = spip_fetch_array($res)){
 			
@@ -99,45 +136,54 @@ function suggestions($txt) {
 		$nom[$row['idp2']] = $row['p2'];
 		
 		/* 3 cas possibles : NULL, 0 ou 1  */
+		/* Astuce pour retrouver les discordants: 
+		  utiliser les nombres premiers */
+		
 		if (!isset($liste[$row['idp1']])) $liste[$row['idp1']] = array();
 		if (!isset($liste[$row['idp1']][$row['idp2']])) 
 			$liste[$row['idp1']][$row['idp2']] = array();
+		
 		if (!isset($liste[$row['idp1']][$row['idp2']])) {
-			$s1 = $s2 = null;
-			if (!is_null($row['fleche_sens1'])) $s1 = $row['fleche_sens1'];
-			if (!is_null($row['fleche_sens2'])) $s2 = $row['fleche_sens2'];
-			$liste[$res['idp1']][$row['idp2']] = array('s1' => $s1,'s2' => $s2);
+			$s1 = $s2 = 0;
+			if (!is_null($row['fleche_sens1'])) $s1 = $prem[$row['fleche_sens1']];
+			if (!is_null($row['fleche_sens2'])) $s2 = $prem[$row['fleche_sens2']];
+			$liste[$row['idp1']][$row['idp2']] = array('s1' => $s1,'s2' => $s2);
 		}
 		else {
 			$s1 = $liste[$row['idp1']][$row['idp2']]['s1'];
 			$s2 = $liste[$row['idp1']][$row['idp2']]['s2'];
-			if (!is_null($row['fleche_sens1'])) $s1 += $row['fleche_sens1'];
-			if (!is_null($row['fleche_sens2'])) $s2 += $row['fleche_sens2'];
+			if (!is_null($row['fleche_sens1'])) $s1 += $prem[$row['fleche_sens1']];
+			if (!is_null($row['fleche_sens2'])) $s2 += $prem[$row['fleche_sens2']];
 			$liste[$row['idp1']][$row['idp2']] = array('s1' => $s1,'s2' => $s2);
 		}
 		
 	}
+
+	spip_log($liste);	
 	
 	// Ventilation et calcul des types de rc
 	foreach ($liste as $idp1 => $l_idp1) {
 		foreach ($l_idp1 as $idp2 => $l_idp2) {
-			$t = sizeof($l_idp2);
-			$sens1 = is_null($l_idp2['s1'])?'':
-				(($l_idp2['s1']===0)?'jamais':
-					(($l_idp2['s1']!=$t)?'toujours':
+			$sens1 = (!$l_idp2['s1'])?'':
+				((($l_idp2['s1']%$prem[0])==0)?'jamais':
+					((($l_idp2['s1']%$prem[1])==0)?'toujours':
 						'discordant'
 					)
 				);
-			$sens2 = is_null($l_idp2['s2'])?'':
-				(($l_idp2['s2']===0)?'jamais':
-					(($l_idp2['s2']!=$t)?'toujours':
+			$sens2 = (!$l_idp2['s2'])?'':
+				((($l_idp2['s2']%$prem[0])==0)?'jamais':
+					((($l_idp2['s2']%$prem[1])==0)?'toujours':
 						'discordant'
 					)
 				);
-			$suggestions[] = array('nom1'=>$nom[$idp1], 'nom2'=>$nom[$idp2], 'type_rc1' => $sens1, 'type_rc2' => $sens2);
+			$suggestions[] = array('nom1'=>$nom[$idp1], 'nom2'=>$nom[$idp2], 'type_rc1' => $sens1, 'type_rc2' => $sens2, 'reactivite' => $l_idp2['s1'] + $l_idp2['s2']);
 		}
 	}
 
+	/* Ensuite on ordonne la liste des suggestions par "réactivité" inverse, en utilisant notre pondération */
+	$suggestions = msort($suggestions, "reactivite", false);
+	
+	spip_log($suggestions);
 	
 	return json_encode($suggestions);
 	
