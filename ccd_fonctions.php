bcb8f39c2e49a8e38e84eb8326798c06671402bc
gilles <gilles>
2007-12-07 02:36:28 +0000

    effacer les produits actifs
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@55 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- ccd_fonctions.php
+++ ccd_fonctions.php
@@ -65,7 +65,7 @@ function ccd($txt) {
 	foreach ($liste_mol_ccd as $index => $nom) {
 		$final[] = array(
 			'item' => $nom, 
-			'link' => "<a id=\"infos_ccd_mol_".$index."\" class=\"info_fm\" href=\"javascript:void(0)\" onclick=\"$('input[@value=".$index."]').next('.x-form-text').toggleClass('actif');return false;\"></a>",
+			'link' => "<a id=\"infos_ccd_mol_".$index."\" class=\"info_fm\" href=\"javascript:void(0)\" onclick=\"$('.actif').addClass('actif');$('input[@value=".$index."]').next('.x-form-text').addClass('actif');return false;\"></a>",
 			'type' => _T("js_mol_title")
 		);
 	}
