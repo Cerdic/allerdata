fd66accb3fb4698a9a9ae22e54453e55a335c360
gilles <gilles>
2007-12-08 05:40:48 +0000

    popup sur les RC avec onglet pour la biblio et le détail d'un produit
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@56 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- bdd.php
+++ bdd.php
@@ -1,6 +1,6 @@
 <?php
 $hostname_allerdata = "localhost";
-$database_allerdata = "db_allerdata";
+$database_allerdata = "allerdata3";
 $username_allerdata = "root";
 $password_allerdata = "";
 $allerdata = mysql_pconnect($hostname_allerdata, $username_allerdata, $password_allerdata) or trigger_error(mysql_error(),E_USER_ERROR); 
