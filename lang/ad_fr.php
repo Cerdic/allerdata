8c45b13b4f2e13dc80ecb8f7be3ec1339e94cbe6
jacques <jacques>
2007-12-12 12:53:05 +0000

    Mise en fichier de langue de la noisette d'inscription
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@73 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- lang/ad_fr.php
+++ lang/ad_fr.php
@@ -6,7 +6,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'accueil' => 'Accueil',
 'accueil_ad' => 'Accueil Allerdata',
 'articlesconnexes' => 'Articles connexes',
-'memerubrique' => 'Dans la m&ecirc;me rubrique'
+'memerubrique' => 'Dans la m&ecirc;me rubrique',
+'acces_reserve_abonnes' => ' acc&egrave;s r&eacute;serv&eacute; aux abonn&eacute;s',
+'intro_login' => '<p>Vous &ecirc;tes d&eacute;j&agrave; abonn&eacute; ?</p>
+				<p>Afin d\'acc&eacute;der au contenu int&eacute;gral de ce site, identifiez-vous en entrant 
+				ci-dessous le nom d\'utilisateur et le mot de passe qui vous ont &eacute;t&eacute; envoy&eacute;s.</p>',
+'blabla_inscription' => '<p>Ce site est la propri&eacute;t&eacute; exclusive de la soci&eacute;t&eacute; AllerData SARL.</p>
+				<p>Il est exclusivement r&eacute;serv&eacute; aux professionnels.</p>
+				<p>L\'acc&egrave;s &agrave; la partie r&eacute;serv&eacute;e aux abonn&eacute;s ne peut se faire qu\'apr&egrave;s attribution d\'un nom d\'utilisateur et d\'un mot de passe.</p>
+				<p>Si vous souhaitez obtenir vos identifiants, veuillez remplir le formulaire ci-dessous&nbsp;:</p>'
 );
 
 
