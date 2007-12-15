0e70933b6b9e7eedd4e29ed6891ba67973e6c702
gilles <gilles>
2007-12-15 23:25:57 +0000

    boeuf, source, et famille taxonomique sont dans un bateau
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@106 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- php/db_creer_noms_sans_accents.php
+++ php/db_creer_noms_sans_accents.php
@@ -1,13 +1,35 @@
 <?php
 
 	$l = mysql_connect('localhost','jpyrat','allerdata007');
+	
+	mysql_query("ALTER TABLE  `tbl_items`  ADD `nom_sans_accent` VARCHAR( 255 ) NOT NULL AFTER `nom`,
+	ADD  `source` VARCHAR( 100 ) NOT NULL COMMENT  'de type 4' AFTER  `nom_sans_accent` ,
+	ADD  `famille` VARCHAR( 100 ) NOT NULL COMMENT  'de type 2' AFTER  `source`,
+	ADD  `source_sans_accent` VARCHAR( 100 ) NOT NULL AFTER  `source` ;");
+	
 	$q = mysql_query('select nom, id_item from allerdata.tbl_items');
 	while($r = mysql_fetch_assoc($q))	{
 		$chaine = $r['nom'];
-		$chaine = strtr($chaine, 'àâäåãáÂÄÀÅÃÁçÇéèêëÉÊËÈïîìíÏÎÌÍñÑöôóòõÓÔÖÒÕùûüúÜÛÙÚÿ','aaaaaaAAAAAAcCeeeeEEEEiiiiIIIInNoooooOOOOOuuuuUUUUy');
+		$chaine = str_replace('Å“','oe',$chaine);
+		$chaine = strtr($chaine, 'Ã Ã¢Ã¤Ã¥Ã£Ã¡Ã‚Ã„Ã€Ã…ÃƒÃÃ§Ã‡Ã©Ã¨ÃªÃ«Ã‰ÃŠÃ‹ÃˆÃ¯Ã®Ã¬Ã­ÃÃŽÃŒÃÃ±Ã‘Ã¶Ã´Ã³Ã²ÃµÃ“Ã”Ã–Ã’Ã•Ã¹Ã»Ã¼ÃºÃœÃ›Ã™ÃšÃ¿','aaaaaaAAAAAAcCeeeeEEEEiiiiIIIInNoooooOOOOOuuuuUUUUy');
 		mysql_query("update allerdata.tbl_items set nom_sans_accent = '".addslashes($chaine)."' where id_item = ".$r['id_item']);	
 		echo "update tbl_items set nom_sans_accent = '".addslashes($chaine)."' where id_item = ".$r['id_item'].";\n";	
 	}	
 	
-
+	mysql_query("UPDATE `tbl_items` a, tbl_items b, tbl_est_dans ab
+	set a.source = b.nom, a.source_sans_accent = b.nom_sans_accent
+	WHERE 
+	a.id_item = ab.id_item
+	and ab.est_dans_id_item = b.id_item 
+	and b.id_type_item = 4
+	and a.id_type_item IN (3,5)");
+	
+	mysql_query("UPDATE `tbl_items` a, tbl_items b, tbl_est_dans ab
+	set a.famille = b.nom
+	WHERE 
+	a.id_item = ab.id_item
+	and ab.est_dans_id_item = b.id_item 
+	and b.id_type_item = 2
+	and a.id_type_item IN (3,5)");
+	
 ?>
