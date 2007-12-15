3fc0309074678cefdcd1e69da98d246623cb3a45
jacques <jacques>
2007-12-15 13:30:41 +0000

    Presque la fin de #11
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@100 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- popup_item_fonctions.php
+++ popup_item_fonctions.php
@@ -2,39 +2,42 @@
 function listetype5($produits) {
 	if (!is_numeric($produits)) return;
 	
-	$query = "SELECT tbl_items_1.id_item, tbl_items_1.nom
+	$query = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom
 				FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.est_dans_id_item) 
 					INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.id_item = tbl_items_1.id_item
 				WHERE (
 						((tbl_items.id_item)=$produits) 
 						AND ((tbl_items.id_type_item)=3) 
 						AND ((tbl_items_1.id_type_item)=5)
-					);";
+					)
+				ORDER BY tbl_items_1.nom;";
 	$res = spip_query($query);
 	$result = '';
 	while ($row = spip_fetch_array($res)){
 		$result .= '<li>'.$row['nom'];
-		$querysource = "SELECT tbl_items_1.id_item, tbl_items_1.nom
+		$querysource = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom
 						FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
 							INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
 						WHERE (
 							((tbl_items.id_item)=".$row['id_item'].") 
 							AND ((tbl_items_1.id_type_item)=4)
-							);";
+							)
+						ORDER BY tbl_items_1.nom;";
 		$ressource = spip_query($querysource);
 		while ($rowsource = spip_fetch_array($ressource)){
-			$source = '(Source&nbsp;: '.$rowsource['nom'].')';
+			$source .= '(Source&nbsp;: '.$rowsource['nom'].')';
 		}
-		$queryfamille = "SELECT tbl_items_1.id_item, tbl_items_1.nom
+		$queryfamille = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom
 						FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
 							INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
 						WHERE (
 							((tbl_items.id_item)=".$row['id_item'].") 
 							AND ((tbl_items_1.id_type_item)=2)
-							);";
+							)
+						ORDER BY tbl_items_1.nom;";
 		$resfamille = spip_query($queryfamille);
 		while ($rowfamille = spip_fetch_array($resfamille)){
-			$famille = '(Famille taxonomique&nbsp;: '.$rowfamille['nom'].')';
+			$famille .= '(Famille taxonomique&nbsp;: '.$rowfamille['nom'].')';
 		}
 		$result .= $source.'</li>';
 	}
@@ -46,18 +49,40 @@ function source($produits) {
 	if (!is_numeric($produits)) return;
 	
 	$result = '';
-	$querysource = "SELECT tbl_items_1.id_item, tbl_items_1.nom
+	$querysource = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom
 					FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
 						INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
 					WHERE (
 						((tbl_items.id_item)=$produits) 
 						AND ((tbl_items_1.id_type_item)=4)
-						);";
+						)
+					ORDER BY tbl_items_1.nom;";
 	$ressource = spip_query($querysource);
 	while ($rowsource = spip_fetch_array($ressource)){
-		$source = 'Source&nbsp;: '.$rowsource['nom'].')';
+		$source .= '<li>Source&nbsp;: '.$rowsource['nom'].'</li>';
 	}
-	$result .= $source.'</li>';
+	$result .= $source;
+
+	return $result;
+}
+
+function allergenes($produits) {
+	if (!is_numeric($produits)) return;
+	
+	$result = '';
+	$queryallergenes = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom
+					FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.est_dans_id_item) 
+						INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.id_item = tbl_items_1.id_item
+					WHERE (
+						((tbl_items.id_item)=$produits) 
+						AND ((tbl_items_1.id_type_item) IN (7,8))
+						)
+					ORDER BY tbl_items_1.nom;";
+	$resallergenes = spip_query($queryallergenes);
+	while ($rowallergenes = spip_fetch_array($resallergenes)){
+		$allergenes .= '<li>Allerg&eagrave;nes&nbsp;: '.$rowallergenes['nom'].'</li>';
+	}
+	$result .= $allergenes;
 
 	return $result;
 }
