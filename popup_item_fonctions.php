cf950f5e63047eb163a3c18e5a4d73c3d9785c08
jacques <jacques>
2007-12-15 10:24:59 +0000

    Avec les familles taxonomiques
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@98 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- popup_item_fonctions.php
+++ popup_item_fonctions.php
@@ -15,11 +15,26 @@ function listetype5($produits) {
 	while ($row = spip_fetch_array($res)){
 		$result .= '<li>'.$row['nom'];
 		$querysource = "SELECT tbl_items_1.id_item, tbl_items_1.nom
-FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
-WHERE (((tbl_items.id_item)=".$row['id_item'].") AND ((tbl_items_1.id_type_item)=4));";
+						FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
+							INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
+						WHERE (
+							((tbl_items.id_item)=".$row['id_item'].") 
+							AND ((tbl_items_1.id_type_item)=4)
+							);";
 		$ressource = spip_query($querysource);
 		while ($rowsource = spip_fetch_array($ressource)){
 			$source = '(Source&nbsp;: '.$rowsource['nom'].')';
+		}
+		$queryfamille = "SELECT tbl_items_1.id_item, tbl_items_1.nom
+						FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
+							INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
+						WHERE (
+							((tbl_items.id_item)=".$row['id_item'].") 
+							AND ((tbl_items_1.id_type_item)=2)
+							);";
+		$resfamille = spip_query($queryfamille);
+		while ($rowfamille = spip_fetch_array($resfamille)){
+			$famille = '(Famille taxonomique&nbsp;: '.$rowfamille['nom'].')';
 		}
 		$result .= $source.'</li>';
 	}
