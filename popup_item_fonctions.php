f40dcc225f7abfd9ea93149c3ff2240df122d3c9
jacques <jacques>
2007-12-15 14:29:13 +0000

    Un peu de debug (ne pas afficher si nom non renseigné) et d'amélioration de la présentation des sources.
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@102 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- popup_item_fonctions.php
+++ popup_item_fonctions.php
@@ -9,6 +9,7 @@ function listetype5($produits) {
 						((tbl_items.id_item)=$produits) 
 						AND ((tbl_items.id_type_item)=3) 
 						AND ((tbl_items_1.id_type_item)=5)
+						AND ( NOT ISNULL(tbl_items_1.nom))
 					)
 				ORDER BY tbl_items_1.nom;";
 	$res = spip_query($query);
@@ -21,25 +22,40 @@ function listetype5($produits) {
 						WHERE (
 							((tbl_items.id_item)=".$row['id_item'].") 
 							AND ((tbl_items_1.id_type_item)=4)
+							AND ( NOT ISNULL(tbl_items_1.nom))
 							)
 						ORDER BY tbl_items_1.nom;";
 		$ressource = spip_query($querysource);
+		$source ='';
 		while ($rowsource = spip_fetch_array($ressource)){
-			$source .= '(Source&nbsp;: '.$rowsource['nom'].')';
+			if ($source) {
+				$source .= ', '.$rowsource['nom'];
+			} else {
+				$source .= $rowsource['nom'];
+			}
 		}
+		if ($source) $source = '(Source&nbsp;: '.$source.')';
+		$result .= $source;
 		$queryfamille = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom
 						FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
 							INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.est_dans_id_item = tbl_items_1.id_item
 						WHERE (
 							((tbl_items.id_item)=".$row['id_item'].") 
 							AND ((tbl_items_1.id_type_item)=2)
+							AND ( NOT ISNULL(tbl_items_1.nom))
 							)
 						ORDER BY tbl_items_1.nom;";
 		$resfamille = spip_query($queryfamille);
+		$famille ='';
 		while ($rowfamille = spip_fetch_array($resfamille)){
-			$famille .= '(Famille taxonomique&nbsp;: '.$rowfamille['nom'].')';
+			if ($famille) { 
+				$famille .= ', '.$rowfamille['nom'];
+			} else {
+				$famille .= $rowfamille['nom'];
+			}
 		}
-		$result .= $source.'</li>';
+		if ($famille) $famille = '(Famille taxonomique&nbsp;: '.$famille.')';
+		$result .= $famille.'</li>';
 	}
 
 	return $result;
@@ -55,6 +71,7 @@ function source($produits) {
 					WHERE (
 						((tbl_items.id_item)=$produits) 
 						AND ((tbl_items_1.id_type_item)=4)
+						AND ( NOT ISNULL(tbl_items_1.nom))
 						)
 					ORDER BY tbl_items_1.nom;";
 	$ressource = spip_query($querysource);
@@ -70,20 +87,21 @@ function allergenes($produits) {
 	if (!is_numeric($produits)) return;
 	
 	$result = '';
-	$queryallergenes = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom, tbl_items_1.masse, tbl_items_1.iuis, tbl_items_1.glyco, tbl_niveaux_allergenicite.niveau_de_preuve
+	$queryallergenes = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom, tbl_items_1.masse, tbl_items_1.iuis, tbl_items_1.glyco, tbl_items_1.fonction_classification, tbl_niveaux_allergenicite.niveau_de_preuve
 					FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.est_dans_id_item) 
 						INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.id_item = tbl_items_1.id_item
 						INNER JOIN tbl_niveaux_allergenicite ON tbl_items_1.id_niveau_allergenicite = tbl_niveaux_allergenicite.id_niveau_allergenicite
 					WHERE (
 						((tbl_items.id_item)=$produits) 
 						AND ((tbl_items_1.id_type_item) IN (7,8))
+						AND ( NOT ISNULL(tbl_items_1.nom))
 						)
 					ORDER BY tbl_items_1.nom;";
 	$resallergenes = spip_query($queryallergenes);
 	while ($rowallergenes = spip_fetch_array($resallergenes)){
 		$allergenes .= '						<tr>
 						<td>'.$rowallergenes['nom'].'</td>
-						<td>???</td>
+						<td>'.$rowallergenes['fonction_classification'].'</td>
 						<td>'.$rowallergenes['masse'].'</td>
 						<td>'.$rowallergenes['iuis'].'</td>
 						<td>'.$rowallergenes['glyco'].'</td>
