3135d980841ca2391d3dee986ab1392fecb72ba4
gilles <gilles>
2007-12-16 01:37:50 +0000

    finalement, si on a trop de rsultat, on plombe la navigateur. Se limiter donc  20 (+ bugs)
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@109 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- action/liste_des_produits.php
+++ action/liste_des_produits.php
@@ -29,17 +29,20 @@ function action_liste_des_produits() {
 	if (is_array($_SESSION['produits_choisis'])) $liste_noire = $_SESSION['produits_choisis'];
 	while ($row = spip_fetch_array($q)) {$res[] = $row; $liste_noire[] = $row['id_item'];}
 	
-	// On complète par une recherche plus large
-	$sql = "select id_item, nom from tbl_items 
-				where id_type_item in (5,3) ";
-	if ($liste_noire)	$sql .="and id_item NOT IN(".implode(',',$liste_noire).")";
-	$sql .= "	and ( nom_sans_accent like '%".addslashes($chaine)."%'
-		or source_sans_accent like '%".addslashes($chaine)."%')";
-	$q = spip_query($sql);
+	// On complète par une recherche plus large (20 maxi)
+	if ($nb_elements_trouves<20) {
+		$sql = "select id_item, nom from tbl_items 
+					where id_type_item in (5,3) ";
+		if ($liste_noire)	$sql .="and id_item NOT IN(".implode(',',$liste_noire).")";
+		$sql .= "	and ( nom_sans_accent like '%".addslashes($chaine)."%'
+			or source_sans_accent like '%".addslashes($chaine)."%')
+			LIMIT 0,".(20-$nb_elements_trouves);
+		$q = spip_query($sql);
 
-	$nb_elements_trouves += spip_num_rows($q);
+		$nb_elements_trouves += spip_num_rows($q);
 
-	while ($row = spip_fetch_array($q)) {$res[] = $row;}
+		while ($row = spip_fetch_array($q)) {$res[] = $row;}
+	}
 
 
 	if (!$nb_elements_trouves) echo('{produits:'.json_encode(array(array('id_item' => 0, 'nom' => _T('nothing_found')))).'}');
