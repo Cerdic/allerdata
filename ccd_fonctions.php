641f5aeb832785fc95a2c3f15a2dd9738503d9db
gilles <gilles>
2007-12-23 10:49:16 +0000

    copie de la requete utilisee dans la popup des CCD. Ne corrige cependant pas le probleme du decompte des produits et pas des molecules
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@185 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- ccd_fonctions.php
+++ ccd_fonctions.php
@@ -27,10 +27,11 @@ function ccd($txt) {
 	
 	$tt = '';
 	
-	$query = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom, tbl_est_dans.est_dans_id_item, tbl_items.id_item as id_mol, tbl_items.nom AS nom2, tbl_items.glyco
-		FROM ((tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
+	$query = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom, tbl_est_dans.est_dans_id_item, tbl_items.id_item AS id_mol, tbl_items.nom AS nom2, tbl_items.glyco, tbl_items_2.nom AS produit
+		FROM (((tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.id_item) 
 			INNER JOIN tbl_est_dans AS tbl_est_dans_1 ON tbl_items.id_item = tbl_est_dans_1.id_item) 
-			INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans_1.est_dans_id_item = tbl_items_1.id_item
+			INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans_1.est_dans_id_item = tbl_items_1.id_item) 
+			INNER JOIN tbl_items AS tbl_items_2 ON tbl_est_dans.est_dans_id_item = tbl_items_2.id_item
 		WHERE (((tbl_est_dans.est_dans_id_item) In ($produits)) AND ((tbl_items_1.id_type_item)=6))
 		ORDER BY tbl_items_1.nom, tbl_est_dans.est_dans_id_item DESC;";
 			
