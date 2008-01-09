e4355cc4ee5d0bb9bb7bc006bed0a14c21345b38
gilles <gilles>
2008-01-09 04:23:49 +0000

    ajout de la bibliographie en bas de page comme demande dans des mails du 21/12
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@271 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

---
+++ js/biblio_footnotes.js
@@ -0,0 +1,12 @@
+$().ready(function footnotes()
+{
+	var notes = 0;
+	
+	$('img.biblio','#main').each( function() {
+			notes++;
+			$('#footnotes').append("<div class='footnote'><a name='note"+notes+"'></a>" + notes + ". " + $(this).attr('title') + " <a href='#nlink"+notes+"' title='retour au texte'>&#8617;</a></div>");
+			$(this).wrap("<a name='nlink"+notes+"' href='#note"+notes+"'></a>");
+		}
+	);
+		
+});
