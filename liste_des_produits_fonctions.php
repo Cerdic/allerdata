39eacb2bd3b8487f19d121120cd3fbbfbf5b5adf
gilles <gilles>
2008-01-09 22:04:14 +0000

    algorithme simplifié de proposition des produits
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@278 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- liste_des_produits_fonctions.php
+++ liste_des_produits_fonctions.php
@@ -11,42 +11,22 @@ function produits_suggeres($query) {
 	
 	$nb_elements_retournes = 20;
 	$nb_elements_trouves = 0;
+	$res = $ids = array();
 	
 	session_start();
 	if (is_array($_SESSION['produits_choisis']))
 		$produits_deja_choisis = implode(",", $_SESSION['produits_choisis']);
 	session_write_close();
 	
-	$sql = "SELECT tbl_items.id_item, nom, source, famille, pos, CONCAT(IF(nom IS NULL, '', CONCAT(nom,'zzz')),source) AS chaine 
-			FROM tbl_items, tbl_index_items
-			WHERE id_type_item IN (5,3) 
-			AND tbl_items.id_item = tbl_index_items.id_item";
-	if ($produits_deja_choisis)	$sql .=" AND tbl_items.id_item NOT IN(".$produits_deja_choisis.")";
-	$sql .= "	AND keyword like '".addslashes($query)."%'";
-	$q = spip_query($sql);
-	
-	$nb_elements_trouves = spip_num_rows($q);
-	
-	$res = $ids = array();
-
 	$liste_noire = array();
 	if (is_array($_SESSION['produits_choisis'])) $liste_noire = $_SESSION['produits_choisis'];
-
-	while ($row = spip_fetch_array($q)) {
-		if (!$row['nom']) $row['nom'] = $row['source'];
-		if (!in_array($row['id_item'],$liste_noire)) {
-			$res[] = $row; 
-			$liste_noire[] = $row['id_item'];
-		}
-	}
 	
 	// On complète par une recherche sans accent
-	$sql = "SELECT tbl_items.id_item, nom, source, famille, pos, CONCAT(IF(nom IS NULL, '', CONCAT(nom,'zzz')),source) AS chaine 
-			FROM tbl_items, tbl_index_items 
-			WHERE id_type_item IN (5,3) 
-			AND tbl_items.id_item = tbl_index_items.id_item";
+	$sql = "SELECT tbl_items.id_item, nom, source, famille
+			FROM tbl_items 
+			WHERE id_type_item IN (5,3) ";
 	if ($liste_noire)	$sql .=" AND tbl_items.id_item NOT IN(".implode(',',$liste_noire).")";
-	$sql .= "	AND keyword like '".addslashes($chaine)."%'";
+	$sql .= "	AND chaine_alpha like '".addslashes($chaine)."%'";
 	$q = spip_query($sql);
 
 	$nb_elements_trouves += spip_num_rows($q);
@@ -61,34 +41,11 @@ function produits_suggeres($query) {
 
 	// On complète par une recherche plus large (20 maxi, question perfs client)
 	if ($nb_elements_trouves<20) {
-		$sql = "SELECT tbl_items.id_item, nom, source, famille, pos, CONCAT(IF(nom IS NULL, '', CONCAT(nom,'zzz')),source) AS chaine 
-				FROM tbl_items, tbl_index_items 
-				WHERE id_type_item IN (5,3) 
-				AND tbl_items.id_item = tbl_index_items.id_item";
+		$sql = "SELECT tbl_items.id_item, nom, source, famille
+				FROM tbl_items 
+				WHERE id_type_item IN (5,3)"; 
 		if ($liste_noire)	$sql .=" AND tbl_items.id_item NOT IN(".implode(',',$liste_noire).")";
-		$sql .= "	AND keyword like '%".addslashes($query)."%'
-			LIMIT 0,".(20-$nb_elements_trouves);
-		$q = spip_query($sql);
-
-		$nb_elements_trouves += spip_num_rows($q);
-
-		while ($row = spip_fetch_array($q)) {
-			if (!$row['nom']) $row['nom'] = $row['source'];
-			if (!in_array($row['id_item'],$liste_noire)) {
-				$res[] = $row; 
-				$liste_noire[] = $row['id_item'];
-			}
-		}
-	}
-
-	// On complète par une recherche plus large (20 maxi, question perfs client)
-	if ($nb_elements_trouves<20) {
-		$sql = "SELECT tbl_items.id_item, nom, source, famille, pos, CONCAT(IF(nom IS NULL, '', CONCAT(nom,'zzz')),source) AS chaine 
-				FROM tbl_items, tbl_index_items 
-				WHERE id_type_item IN (5,3) 
-				AND tbl_items.id_item = tbl_index_items.id_item";
-		if ($liste_noire)	$sql .=" AND tbl_items.id_item NOT IN(".implode(',',$liste_noire).")";
-		$sql .= "	AND keyword like '%".addslashes($chaine)."%'
+		$sql .= "	AND chaine_alpha like '%".addslashes($chaine)."%'
 			LIMIT 0,".(20-$nb_elements_trouves);
 		$q = spip_query($sql);
 
