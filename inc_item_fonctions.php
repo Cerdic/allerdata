c12282af459fae0ce7adb08bef891e55f77224a4
jacques <jacques>
2008-05-13 17:12:11 +0000

    Première étape pour #160 : retour de tous les items, testables ou non dans le popup.
    Ce qui fait que les items de type 9 n'apparaissaient pas, c'est que leur niveau de preuve n'était jamais renseigné.
    
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@424 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- inc_item_fonctions.php
+++ inc_item_fonctions.php
@@ -107,16 +107,16 @@ function source($produits) {
 	return implode(", ", $t_nom_sources);
 }
 
-function allergenes($produits, $types = '7,8') {
+function allergenes($produits, $types = '7,8,9') {
 	if (!is_numeric($produits)) return;
 	
 	$result = '';
 	$queryallergenes = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom, tbl_items_1.masse, tbl_items_1.iuis, tbl_items_1.glyco, tbl_items_1.testable, tbl_items_1.fonction_classification, tbl_niveaux_allergenicite.niveau_de_preuve
-					FROM (tbl_items INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.est_dans_id_item) 
-						INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.id_item = tbl_items_1.id_item
-						INNER JOIN tbl_niveaux_allergenicite ON tbl_items_1.id_niveau_allergenicite = tbl_niveaux_allergenicite.id_niveau_allergenicite
+					 FROM (tbl_est_dans 
+						INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.id_item = tbl_items_1.id_item) 
+						LEFT JOIN tbl_niveaux_allergenicite ON tbl_items_1.id_niveau_allergenicite = tbl_niveaux_allergenicite.id_niveau_allergenicite
 					WHERE (
-						((tbl_items.id_item)=$produits) 
+						((tbl_est_dans.est_dans_id_item)=$produits) 
 						AND ((tbl_items_1.id_type_item) IN ($types))
 						AND ( NOT ISNULL(tbl_items_1.nom))
 						)
@@ -129,9 +129,9 @@ function allergenes($produits, $types = '7,8') {
 						<td>'.(($rowallergenes['fonction_classification'] == $rowallergenes['nom']) ? '':$rowallergenes['nom']).'</td>
 						<td>'.$rowallergenes['fonction_classification'].'</td>
 						<td style="text-align:right;">'.$rowallergenes['masse'].'</td>
-						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['iuis']==1)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
-						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['glyco']==-1)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
-						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['testable']==-1)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
+						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['iuis']<>0)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
+						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['glyco']<>0)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
+						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['testable']<>0)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
 						</tr>';
 	}
 	$result .= $allergenes;
