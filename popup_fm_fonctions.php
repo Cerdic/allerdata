67e821a02d6ed05c0c06e0e2da05e5f0238269ea
jacques <jacques>
2008-05-14 09:02:04 +0000

    Quelques ajustements suites à divers mails.
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@430 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- popup_fm_fonctions.php
+++ popup_fm_fonctions.php
@@ -83,22 +83,28 @@ function allergenes_testables($produits) {
 	if (!is_numeric($produits)) return;
 	
 	$result = '';
-	$queryallergenes = "SELECT DISTINCT tbl_items_1.id_item, tbl_items_1.nom_complet, tbl_items_1.masse, tbl_items_1.iuis, tbl_items_1.glyco, tbl_items_1.testable, tbl_items_1.fonction_classification
-					 FROM (tbl_est_dans 
-						INNER JOIN tbl_items AS tbl_items_1 ON tbl_est_dans.id_item = tbl_items_1.id_item) 
+	$queryallergenes = "SELECT tbl_items_2.nom_court AS nom_produit, tbl_items_2.source AS source_produit, tbl_items_1.id_item, tbl_items_1.nom, tbl_items_1.masse, tbl_items_1.iuis, tbl_items_1.glyco, tbl_items_2.id_type_item
+						FROM (tbl_items 
+							INNER JOIN tbl_est_dans ON tbl_items.id_item = tbl_est_dans.est_dans_id_item) 
+								INNER JOIN ((tbl_items AS tbl_items_1 
+									INNER JOIN tbl_est_dans AS tbl_est_dans_2 ON tbl_items_1.id_item = tbl_est_dans_2.id_item) 
+										INNER JOIN tbl_items AS tbl_items_2 ON tbl_est_dans_2.est_dans_id_item = tbl_items_2.id_item) ON tbl_est_dans.id_item = tbl_items_1.id_item 
 					WHERE (
 						((tbl_est_dans.est_dans_id_item)=$produits) 
 						AND ((tbl_items_1.testable) <> 0)
 						AND ((tbl_items_1.id_type_item) IN (7,8,9,10))
-						AND ( NOT ISNULL(tbl_items_1.nom_complet))
+						AND ( NOT ISNULL(tbl_items_1.nom))
+						/*AND ((tbl_items_2.id_type_item) In (5))*/
 						)
-					ORDER BY tbl_items_1.nom_complet;";
+					ORDER BY tbl_items_1.nom, tbl_items_2.source;";
 	$resallergenes = spip_query($queryallergenes);
 	$count = 0;
 	while ($rowallergenes = spip_fetch_array($resallergenes)){
 		$count += 1;
 		$allergenes .= '						<tr'.((($count % 2) == 0)?' class="row_even"':' class="row_odd"').'>
-						<td>'.$rowallergenes['nom_complet'].'</td>
+						<td>'.$rowallergenes['nom'].'</td>
+						<td>'.$rowallergenes['nom_produit'].'</td>
+						<td>'.$rowallergenes['source_produit'].'</td>
 						<td style="text-align:center;font-size:1.5em;">'.(($rowallergenes['glyco']<>0)?'<img src="squelettes/img/icon_accept.gif" />':'').'</td>
 						</tr>';
 	}
