7535fc083c64fd671f9ac61c5e59036acf8ffd3b
Cerdic <cedric@yterium.com>
2008-10-09 10:26:31 +0000

    edition des allergenes
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@464 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

---
+++ plugins/allergenes/formulaires/editer_allergene.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Plugin allerdata / Admin du site
+ * Licence GPL
+ * (c) 2008 C.Morin Yterium pour Allerdata SARL
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+include_spip('inc/actions');
+include_spip('inc/editer');
+
+function formulaires_editer_allergene_charger_dist($id_item='new', $id_parent=0, $retour='', $lier=0, $config_fonc='', $row=array(), $hidden=''){
+	$valeurs = formulaires_editer_objet_charger('tbl_item',$id_item,$id_parent,$lier,$retour,$config_fonc,$row,$hidden);
+	$valeurs['commentaires'] = '';
+
+	include_spip('inc/allerdata_arbo');
+	$valeurs['id_parent'] = allerdata_les_parents($id_item);
+	if (!count($valeurs['id_parent'])) 
+		$valeurs['id_parent'] =	array($id_parent);
+	return $valeurs;
+}
+
+function formulaires_editer_allergene_verifier_dist($id_item='new', $id_parent=0, $retour='', $lier=0, $config_fonc='', $row=array(), $hidden=''){
+	$erreurs = formulaires_editer_objet_verifier('tbl_item',$id_item,array('nom','commentaires'));
+	
+	return $erreurs;
+}
+
+
+function formulaires_editer_allergene_traiter_dist($id_item='new', $id_parent=0, $retour='', $lier=0, $config_fonc='', $row=array(), $hidden=''){
+	foreach(array('ccd_possible','testable','iuis','glyco') as $check)
+	if (!_request($check))
+		set_request($check,0);
+	// vilain hack
+	set_request('action','editer_tbl_item');
+	// hop traitons tout cela
+	return formulaires_editer_objet_traiter('tbl_item',$id_item,$id_parent,$lier,$retour,$config_fonc,$row,$hidden);
+}
+
+
+?>
---
+++ plugins/allergenes/lang/editer_allergene_fr.php
@@ -0,0 +1,22 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'label_id_parent_source' => 'Source',
+'label_id_parent_produit' => 'Produit',
+'label_id_parent_famille_mol' => 'Famille mol&eacute;culaire',
+'label_id_type_item' => 'Type item',
+'label_nom' => 'Nom',
+'label_masse' => 'Masse',
+'label_iuis' => 'IUIS',
+'label_glyco' => 'Glycosyl&eacute;',
+'label_fonction_classification' => 'Fonction',
+'label_testable' => 'Testable <i>in vitro</i>',
+'label_ccd_possible' => 'CCD possible',
+'label_remarques' => 'Remarques',
+'label_id_niveau_allergenicite' => 'Niveau allerg&eacute;nicit&eacute;',
+
+
+);
+?>
