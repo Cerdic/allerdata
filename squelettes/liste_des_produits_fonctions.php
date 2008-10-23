a8bf966216f2e8f41e068f7a3eb4b4dda24fdc74
Cerdic <cedric@yterium.com>
2008-10-23 18:08:52 +0000

    une squeletisation qu'il a fallu aller chercher loin : - passage de la requette ajax json en GET au lieu de POST pour beneficier du cache - exclusion des _dc du cache (ce sont les timestamp que ext met dans toutes les requetes ajax pour eviter les pb de cache navigateur) - passage des produits deja charges en argument de la requete ajax, pour impacter le cache (ils etaient caches en $_SESSION auparavant) - ecriture en boucle au lieu de requetes sql dans le texte (#195)
    Au passage on n'utilise plus les champs source et famille, mais on prefere faire une requete supplementaire en boucle.
    Du coup, la presentation des suggestion de produits est nettement accelere, y compris lors d'un calcul
    
    
    
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@492 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- squelettes/liste_des_produits_fonctions.php
+++ squelettes/liste_des_produits_fonctions.php
@@ -1,89 +1,17 @@
 <?php
+include_spip('inc/json');
 
-function produits_suggeres($query) {
-		
-	include_spip('inc/charsets');
-	
-	// $query = strtolower($query); // plante sur le serveur de dev de jacques ==> inversion de l'ordre des 2 lignes.
-	$chaine = translitteration($query);
-	$chaine = strtolower($chaine);
-	
-	$nb_elements_retournes = 20;
-	$nb_elements_trouves = 0;
-	$tableau_produits = $liste_noire = $res = $ids = array();
-  $produits_deja_choisis = '';
-	
-  // Exclure les produits déjà dans les zones de saisie du penta
-  // Ainsi que leurs parents dans l'ordre de composition
-  // Cette liste est maintenue lors du calcul des suggestions (et purgée au chargement de la page)
-	session_start();
-	if (isset($_SESSION['produits_choisis']) && is_array($_SESSION['produits_choisis'])) {
-    $liste_noire = $_SESSION['produits_choisis'];
-		$produits_deja_choisis = implode(",", $liste_noire);
-  }
-  
-  session_write_close();
-  echo "/*
-appel le ".date('d/m h:i:s')."
-*/";
-
-	$sql = "SELECT tbl_items.id_item, nom, nom_court, source, famille
-			FROM tbl_items 
-			WHERE id_type_item IN (5,3) ";
-	if ($liste_noire)	$sql .=" AND tbl_items.id_item NOT IN(".implode(',',$liste_noire).")";
-	$sql .= "	AND chaine_alpha like '".addslashes($chaine)."%'";
-	$sql .= " ORDER BY id_type_item , nom";
-	$q = spip_query($sql);
-
-	$nb_elements_trouves += spip_num_rows($q);
-
-	while ($row = spip_fetch_array($q)) {
-		if (!$row['nom']) $row['nom'] = $row['source'];
-		if (!in_array($row['id_item'],$liste_noire)) {
-			$res[] = $row; 
-			$liste_noire[] = $row['id_item'];
-		}
+function exergue($nom,$query){
+	static $chaine = array();
+	if (!isset($chaine[$query])){
+		include_spip('inc/charsets');
+		$chaine[$query] = translitteration($query);
+		$chaine[$query] = strtolower($chaine[$query]);
 	}
-
-	// On complete par une recherche plus large (20 maxi, question perfs client)
-	if ($nb_elements_trouves<20) {
-		$sql = "SELECT tbl_items.id_item, nom, nom_court, source, famille
-				FROM tbl_items 
-				WHERE id_type_item IN (5,3)"; 
-		if ($liste_noire)	$sql .=" AND tbl_items.id_item NOT IN(".implode(',',$liste_noire).")";
-		$sql .= "	AND chaine_alpha like '%".addslashes($chaine)."%'
-			ORDER BY nom
-			LIMIT 0,".(20-$nb_elements_trouves);
-		$q = spip_query($sql);
-
-		$nb_elements_trouves += spip_num_rows($q);
-
-		while ($row = spip_fetch_array($q)) {
-			if (!$row['nom']) $row['nom'] = $row['source'];
-			if (!in_array($row['id_item'],$liste_noire)) {
-				$res[] = $row; 
-				$liste_noire[] = $row['id_item'];
-			}
-		}
-	}
-
-	$final = $pos = $nom = $items = array();
-	
-	//Mise en avant
-	foreach ($res as $key => $row) {
-		$row['nom_mis_en_forme'] = $row['nom'];
-		if ($query!=$chaine) $row['nom_mis_en_forme'] = eregi_replace($query,'<b>'.$query.'</b>',$row['nom_mis_en_forme']);
-		$row['nom_mis_en_forme'] = eregi_replace($chaine,'<b>'.$chaine.'</b>',$row['nom_mis_en_forme']);
-		if ($query!=$chaine) $row['source'] = eregi_replace($query,'<b>'.$query.'</b>',$row['source']);
-		$row['source'] = eregi_replace($chaine,'<b>'.$chaine.'</b>',$row['source']);		
-		$final[$key] = $row;
-	}
-
-	if (_request('debug')) {var_dump($final);die();}
-	
-	if (!$nb_elements_trouves) return(json_encode(array(array('id_item' => '', 'nom' => '', 'nom_mis_en_forme' => '', 'source' => _T('ad:nothing_found').' pour '.$chaine))));
-	else return(json_encode($final));
-
+	if ($query!=$chaine[$query])
+		$nom = preg_replace("/".preg_quote($query)."/i",'<b>'.$query.'</b>',$nom);
+	$nom = preg_replace("/".preg_quote($chaine[$query])."/i",'<b>'.$chaine[$query].'</b>',$nom);
+	return $nom;
 }
-	
+
 ?>
--- squelettes/mes_options.php
+++ squelettes/mes_options.php
@@ -5,10 +5,10 @@ define('_ID_WEBMESTRES', '1:2');
 $GLOBALS['ligne_horizontale'] = "\n<div class='hrspip'><hr /></div>\n";
 
 # patch compatibilite vieux plugins
-#$table_des_traitements['TITRE'][]= 'typo(trim(supprimer_numero(%s)))';
-define('_FEED_GLOBALS', true); 
+$table_des_traitements['TITRE'][]= 'typo(trim(supprimer_numero(%s)))';
+define('_FEED_GLOBALS', true);
 #$GLOBALS['type_urls'] = 'propres2';
-#include_spip('inc/vieilles_defs');
+include_spip('inc/vieilles_defs');
 
 # patch compatibilite php < 5.1
 if (!function_exists('json_encode')){
@@ -17,5 +17,8 @@ if (!function_exists('json_encode')){
 		return json_export($var);
 	}
 }
+
+@define('_CONTEXTE_IGNORE_VARIABLES',"/(^var_|^PHPSESSID$|^_dc$)/");
+
 
 ?>
