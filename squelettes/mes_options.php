1e33e4d49d81e9843d489cefcc38ba7f229e4c80
Cerdic <cedric@yterium.com>
2008-10-24 08:51:02 +0000

    tout ce qui concerne l'outil a ete demenage dans plugins/outil dans [496]
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@497 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- squelettes/mes_options.php
+++ squelettes/mes_options.php
@@ -6,19 +6,8 @@ $GLOBALS['ligne_horizontale'] = "\n<div class='hrspip'><hr /></div>\n";
 
 # patch compatibilite vieux plugins
 $table_des_traitements['TITRE'][]= 'typo(trim(supprimer_numero(%s)))';
-define('_FEED_GLOBALS', true);
+
 #$GLOBALS['type_urls'] = 'propres2';
-include_spip('inc/vieilles_defs');
-
-# patch compatibilite php < 5.1
-if (!function_exists('json_encode')){
-	function json_encode($var){
-		include_spip('inc/json');
-		return json_export($var);
-	}
-}
-
-@define('_CONTEXTE_IGNORE_VARIABLES',"/(^var_|^PHPSESSID$|^_dc$)/");
 
 
 ?>
