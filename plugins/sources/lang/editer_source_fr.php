fa261eff0a573ba8f2435c74b0eb64ab031e6454
Cerdic <cedric@yterium.com>
2009-01-16 09:26:04 +0000

    "#204 : homogeneite des label"
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@595 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- plugins/sources/lang/editer_source_fr.php
+++ plugins/sources/lang/editer_source_fr.php
@@ -4,8 +4,8 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 'label_id_parent' => 'Famille taxonomique',
-'label_nom' => 'Nom latin correct',
-'label_autre_nom' => 'Nom latin invalide',
+'label_nom' => 'Nom latin',
+'label_autre_nom' => 'Nom anciennement utilis&eacute;',
 'label_remarques' => 'Remarques',
 
 'item_deja_existant_avec_meme_nom' => 'Il existe d&eacute;j&agrave; une source avec ce nom&nbsp;: ',
