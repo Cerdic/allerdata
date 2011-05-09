993394f36bc9d2914a538345789e00be1837ba6f
Cerdic <cedric@yterium.com>
2011-05-09 13:52:58 +0000

    i18n : chaine de langue
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@809 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- squelettes/lang/local_fr.php
+++ squelettes/lang/local_fr.php
@@ -4,75 +4,58 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // A
-'accueil' => "Accueil",
-'allergenicite' => "Allergenicit&eacute;",
-'assistant_moleculaire' => "Outil",
-'aucun_resultat' => "Aucun r&eacute;sultat",
-'ajoute_le' => 'Ajout&eacute; le',
+
+
 
 
 // B
-'by' => "par",
+//'by' => "par",
 
 
 // C
-'chaine_alpha' => "Chaine alpha",
+//'chaine_alpha' => "Chaine alpha",
 
 // F
-'form_forum_indiquer_nom_email' => 'Indiquez ici votre nom et votre adresse email. Votre identifiant personnel vous parviendra rapidement, par courrier &eacute;lectronique.',
-'full_name' => "Nom complet",
+//'form_forum_indiquer_nom_email' => 'Indiquez ici votre nom et votre adresse email. Votre identifiant personnel vous parviendra rapidement, par courrier &eacute;lectronique.',
+//'full_name' => "Nom complet",
 
 
-// G
-'glyco' => "Glyco",
-'get_url' => "Obtenir l'adresse de cette page",
 
 // H
-'h1_acces_restreint' => "L'acc&egrave;s &agrave; cette zone est restreint",
-'help_needed' => "Besoin d'Aide ?",
+//'h1_acces_restreint' => "L'acc&egrave;s &agrave; cette zone est restreint",
 
 // I
-'instructions_login' => "Afin d'acc&eacute;der au contenu prot&eacute;g&eacute; de ce site, identifiez-vous en entrant 
-				ci-dessous le nom d'utilisateur et le mot de passe qui vous ont &eacute;t&eacute; envoy&eacute;s !",
-'interrogeable' => "Interrogeable",
-'iuis' => "IUIS",
+/*'instructions_login' => "Afin d'acc&eacute;der au contenu prot&eacute;g&eacute; de ce site, identifiez-vous en entrant
+				ci-dessous le nom d'utilisateur et le mot de passe qui vous ont &eacute;t&eacute; envoy&eacute;s !",*/
+//'interrogeable' => "Interrogeable",
+
 
 // J
 'jaime' => 'Selectionner',
 'jaimeplus' => 'Retirer',
 
 //L
-'loading' => "Chargement",
 'login_login' => "Nom d'utilisateur&nbsp;:",
 'login_login2' => "Nom d'utilisateur&nbsp;:",
 
 
 // M
-'masse' => "Masse",
-'mon_profil' => 'Mes favoris',
+//'masse' => "Masse",
 'zmes_favoris' => 'Ma selection d\'articles',
 
 
-//N
-'name' => "Nom",
 
 // O
-'other_name' => "Autre Nom",
+//'other_name' => "Autre Nom",
 
 
 // P
 // Anciens ajouts accessibilite Agora (non implemente actuellement)
-'pass_forum_bla' => 'Vous avez demand&eacute; &agrave; consulter un article
-		r&eacute;serv&eacute; aux allergologues.',
-
-		
-// R
-'reactivites_croisees' => "R&eacute;activit&eacute; crois&eacute;e",
+'pass_forum_bla' => 'Vous avez demand&eacute; &agrave; consulter un article r&eacute;serv&eacute; aux allergologues.',
 
 
 // T
-'testable' => "Testable",
-'test_code' => "Code test",
+//'test_code' => "Code test",
 
 );
 ?>
