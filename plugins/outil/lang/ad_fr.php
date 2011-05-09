2454fd4aa6080078824d1c6aed214c2a3b93f730
Cerdic <cedric@yterium.com>
2011-05-09 13:52:47 +0000

    i18n : chaine de langue
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@808 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- plugins/outil/lang/ad_fr.php
+++ plugins/outil/lang/ad_fr.php
@@ -5,52 +5,85 @@
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'accueil' => 'Accueil',
 	'accueil_ad' => 'Accueil Allerdata',
+	'allergenicite' => "Allergenicit&eacute;",
+	'annee' => 'année',
+	'assistant_moleculaire' => "Outil",
+	'aucun_resultat' => "Aucun r&eacute;sultat",
+	'ajoute_le' => 'Ajout&eacute; le',
+
 	'articlesconnexes' => 'Articles connexes',
-	'acces_reserve_abonnes' => '<br />Page &agrave; acc&egrave;s r&eacute;serv&eacute; aux abonn&eacute;s',
-	'aucune_etude_de_ce_type' => 'Aucune &eacute;tude de ce type',
+	'acces_reserve_abonnes' => '<br />Page à accès réservé aux abonnés',
+	'aucune_etude_de_ce_type' => 'Aucune étude de ce type',
 	
 	// B
-	'blabla_inscription' => '<p>Ce site est la propri&eacute;t&eacute; exclusive de la soci&eacute;t&eacute; AllerData SARL.</p>
-					<p>Il contient des informations qui, mal interpr&eacute;t&eacute;es, pourraient conduire &agrave; un diagnostic et une prise en charge erron&eacute;s.</p>
-					<p>C\'est pour cela qu\'il est exclusivement r&eacute;serv&eacute; aux professionnels de sant&eacute; comp&eacute;tents en allergologie apr&egrave;s v&eacute;rification de leurs donn&eacute;es.</p>
-					<p>Si vous &ecirc;tes allergologue ou travaillez dans le domaine de l\'allergie, vous pouvez demander &agrave; b&eacute;n&eacute;ficier d\'un acc&egrave;s complet en remplissant le formulaire ci-dessous.</p>',
-					
+	'bibliographie' => 'Bibliographie',
+	'blabla_inscription' => '<p>Ce site est la propriété exclusive de la société AllerData SARL.</p>
+					<p>Il contient des informations qui, mal interprétées, pourraient conduire à un diagnostic et une prise en charge erronés.</p>
+					<p>C\'est pour cela qu\'il est exclusivement réservé aux professionnels de santé compétents en allergologie après vérification de leurs données.</p>
+					<p>Si vous êtes allergologue ou travaillez dans le domaine de l\'allergie, vous pouvez demander à bénéficier d\'un accès complet en remplissant le formulaire ci-dessous.</p>',
+
+	'choisissez' => 'Choisissez',
+	'citation' => 'Citation',
+
+	// G
+	'get_url' => "Obtenir l'adresse de cette page",
+	'glyco' => "Glyco",
+
+	'help_needed' => "Besoin d'Aide ?",
+
 	// I
-	'intro_login' => '<p>Vous &ecirc;tes d&eacute;j&agrave; abonn&eacute; ?</p>
-					<p>Afin d\'acc&eacute;der au contenu int&eacute;gral de ce site, identifiez-vous en entrant 
-					ci-dessous le nom d\'utilisateur et le mot de passe qui vous ont &eacute;t&eacute; envoy&eacute;s.</p>',
-	
+	'intro_login' => '<p>Vous êtes déjà abonné ?</p>
+					<p>Afin d\'accéder au contenu intégral de ce site, identifiez-vous en entrant 
+					ci-dessous le nom d\'utilisateur et le mot de passe qui vous ont été envoyés.</p>',
+	'iuis' => "IUIS",
+
+	'imprimer' => 'imprimer',
+
 	// J	
-	'js_ccd_name' => 'Libell&eacute;',
-	'js_ccd_resultat' => 'R&eacute;sultat',
-	'js_ccd_resultats' => 'R&eacute;sultats',
-	'js_ccd_title' => 'CCD prouv&eacute;s',
+	'js_ccd_name' => 'Libellé',
+	'js_ccd_resultat' => 'Résultat',
+	'js_ccd_resultats' => 'Résultats',
+	'js_ccd_title' => 'CCD prouvés',
 	'js_close_button' => 'Fermer',
 	'js_combo_emptytext' => 'Saisissez une valeur...',
 	'js_combo_loadingtext' => 'Recherche...',
 	'js_error_gotdata' => "la liste des réactivités croisées n'a pas pu être calculée correctement. Veuillez rafraîchir la page ou réinitialiser le pentagramme.", // Garder les accents "en dur" car c'est pour un message d'alerte javascript
-	'js_fm_name' => "Libell&eacute;",
+	'js_fm_name' => "Libellé",
 	'js_fm_nb' => "Nb",
-	'js_fm_grid_title' => 'Familles mol&eacute;culaires',
+	'js_fm_grid_title' => 'Familles moléculaires',
 	'js_load_mask' => "modification en cours...",
-	'js_mol_title' => 'Mol&eacute;cules',
-	'js_sugg_grid_title' => 'Croisent &eacute;galement',
+	'js_mol_title' => 'Molécules',
+	'js_sugg_grid_title' => 'Croisent également',
 	'js_sugg_p' => "Produit",
 	'js_sugg_card' => "Nb",
-	'js_fm_tests' => "Tests biologiques repr&eacute;sentatifs",
+	'js_fm_tests' => "Tests biologiques représentatifs",
 	'js_tests_cliniques_title' => "Tests cliniques",
   'js_titre_onglet_par_defaut' => 'Détail',
   'js_titre_popup_fm' => 'Famille moléculaire',
+
+	// L
+	'loading' => 'Chargement en cours..',
+	'liste_items' => 'Liste des Items',
 	
 	// M
-	'memerubrique' => 'Dans la m&ecirc;me rubrique',
-	
-	// N
-	'nothing_found' => "Aucun &eacute;l&eacute;ment n'a &eacute;t&eacute; trouv&eacute;",
-	
+	'memerubrique' => 'Dans la même rubrique',
+	'mon_profil' => 'Mes favoris',
+
+	//N
+	'name' => "Nom",
+	'nothing_found' => "Aucun élément n'a été trouvé",
+
+
+	// R
+	'reactivites_croisees' => "R&eacute;activit&eacute; crois&eacute;e",
+
 	// T
-	'titre_bibliographies_popup_rc' => 'M&eacute;thodes et r&eacute;sultats',
-	'titre_synthese_popup_rc' => 'Synth&egrave;se des r&eacute;sultats'
+	'titre_bibliographies_popup_rc' => 'Méthodes et résultats',
+	'titre_synthese_popup_rc' => 'Synthèse des résultats',
+	'testable' => "Testable in vitro",
+
+	// W
+	'work_liste_items_interrogeables' => 'La liste des items interrogeables',
 );
 
 
