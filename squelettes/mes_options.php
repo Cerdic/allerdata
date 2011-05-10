f063555940b491c393df22f8c772aaa88701b632
Cerdic <cedric@yterium.com>
2011-05-10 08:37:34 +0000

    i18n : chaines de langue + forcer_lang + cookie lang + #PRODUIRE
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@812 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- squelettes/mes_options.php
+++ squelettes/mes_options.php
@@ -12,4 +12,10 @@ $table_des_traitements['TITRE'][]= 'typo(trim(supprimer_numero(%s)))';
 $GLOBALS['debut_intertitre'] = "<h2 class='spip'>";
 $GLOBALS['fin_intertitre'] = '</h2>';
 
+$GLOBALS['forcer_lang'] = true;
+if ($lang = _request('lang')){
+	include_spip('inc/cookie');
+	spip_setcookie('spip_lang',$_COOKIE['spip_lang']=$lang);
+}
+
 ?>
