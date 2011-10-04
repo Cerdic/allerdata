29751ac3094650fae06ce4c9a30b659f74d5bb66
Cerdic <cedric@yterium.com>
2011-10-04 15:58:22 +0000

    mise a jour de la trad
    git-svn-id: http://svn.yterium.net/svn/allerdata/trunk@827 78a69512-8ef6-46d5-8a56-b2c1bc0f7f57

--- plugins/outil/lang/ad_en.php
+++ plugins/outil/lang/ad_en.php
@@ -11,36 +11,36 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_resultat' => "No Result",
 	'ajoute_le' => 'Added',
 	'acces_libre' => 'Free access',
-	'acces_reserve' => 'Restricted access',
+	'acces_reserve' => 'Limited access',
 
-	'articlesconnexes' => 'Related contents',
-	'acces_reserve_abonnes' => '<br />Restricted access for subscribers only',
-	'aucune_etude_de_ce_type' => 'No such study',
+	'articlesconnexes' => 'Closely related contents',
+	'acces_reserve_abonnes' => '<br />For subscribers only',
+	'aucune_etude_de_ce_type' => 'No study of this kind',
 	
 	// B
 	'bibliographie' => 'Bibliography',
-	'blabla_inscription' => '<p> This site is the exclusive property of the company AllerData LLC. </p>
-<p>It contains information, that may lead to diagnosis and treatment errors if misinterpreted</p>
-<p>This is why it is reserved exclusively for healthcare professionals competent in allergy after verification of their data.</p>
-<p>If you are allergist or working in the field of allergy, you can apply for full access by filling out the form below. </p>',
+	'blabla_inscription' => '<p>This web site is the exclusive property of Allerdata SARL. </p>
+<p>It contains information which, if incorrectly interpreted, can lead to diagnostic and treatment errors. </p>
+<p>For this reason, access to this site is available only to health professionals with documented competence in allergy. </p>
+<p>If you are an allergist or work in the field of allergy, you may apply for full access by filling out the form below. </p>',
 	'bouton_annuler' => 'Cancel',
 	'bouton_valider' => 'OK',
 
-	'choisissez' => 'Pick one',
+	'choisissez' => 'Choose',
 	'citation' => 'Citation',
 
 	'en_savoir_plus' => 'Read more',
 
 	// G
-	'get_url' => "Get URL of this view",
+	'get_url' => "Get URL",
 	'glyco' => "Glyco",
 
-	'help_needed' => "Some Help?",
+	'help_needed' => "Need Help?",
 
 	// I
-	'intro_login' => '<p> Are you already subscribed ?</p>
-<p> To access the full content of this site, please login by entering
-below the user name and password that you have been sent. </p>',
+	'intro_login' => '<p> Do you already have a subscription?</p>
+<p> In order to access the full contents of this site,
+identify yourself by entering your user name and the password which has been sent to you. </p>',
 	'iuis' => "IUIS",
 
 	'imprimer' => 'print',
@@ -49,11 +49,11 @@ below the user name and password that you have been sent. </p>',
 	'js_ccd_name' => 'Designation',
 	'js_ccd_resultat' => 'Result',
 	'js_ccd_resultats' => 'Results',
-	'js_ccd_title' => 'Proven CCD',
+	'js_ccd_title' => 'Proven CCDs',
 	'js_close_button' => 'Close',
 	'js_combo_emptytext' => 'Enter value...',
 	'js_combo_loadingtext' => 'Searching...',
-	'js_error_gotdata' => "The list of cross-reactivity could not be calculated correctly. Please refresh page or reset the pentagram.", // Garder les accents "en dur" car c'est pour un message d'alerte javascript
+	'js_error_gotdata' => "The cross-reactivities list could not be formulated correctly. Please update the page or re-initialize the pentagram.", // Garder les accents "en dur" car c'est pour un message d'alerte javascript
 	'js_fm_name' => "Designation",
 	'js_fm_nb' => "No.",
 	'js_fm_grid_title' => 'Molecular families',
@@ -62,7 +62,7 @@ below the user name and password that you have been sent. </p>',
 	'js_sugg_grid_title' => 'Also crossing',
 	'js_sugg_p' => "Product",
 	'js_sugg_card' => "No.",
-	'js_fm_tests' => "Biological representative tests",
+	'js_fm_tests' => "Representative laboratory tests",
 	'js_tests_cliniques_title' => "Clinical tests",
   'js_titre_onglet_par_defaut' => 'Detail',
   'js_titre_popup_fm' => 'Molecular families',
@@ -74,10 +74,10 @@ below the user name and password that you have been sent. </p>',
 	'lien_login' => 'Login',
 	'lire_la_suite' => 'Read more',
 	'lire_la_suite_edito' => 'Editorial, Read more',
-	'label_les_allergenes_de' => 'Allergens for @nom@',
-	'label_les_allergenes_testables_de' => 'In vitro testable allergens for @nom@',
+	'label_les_allergenes_de' => 'Allergens of @nom@',
+	'label_les_allergenes_testables_de' => 'In vitro testable allergens of @nom@',
 	'label_produit' => 'Product',
-	'label_organisme_source' => 'Source organism',
+	'label_organisme_source' => 'Source material',
 	'label_nom' => 'Name',
 	'label_famille_taxo' => 'Taxonomic family',
 	'label_famille_mol' => 'Molecular family',
@@ -86,13 +86,13 @@ below the user name and password that you have been sent. </p>',
 
 	'label_pays' => 'State',
 	'label_nb_sujets' => 'No. subjects',
-	'label_test_individuels' => 'Individually tested serums',
+	'label_test_individuels' => 'Sera assayed individually',
 	'label_test_quantitatif' => 'Quantitative Test',
 	'label_produit1' => 'Product#1',
 	'label_rc1v2' => 'CR #1-&gt;#2',
 	'label_rc2v1' => 'CR #1&lt;-#2',
 	'label_produit2' => 'Product#2',
-	'label_remarques' => 'Notes',
+	'label_remarques' => 'Remarks',
 	'label_retour_synthese' => 'Back to summary',
 
 	// M
@@ -115,23 +115,23 @@ below the user name and password that you have been sent. </p>',
 	'titre_bibliographies_popup_rc' => 'Methods and results',
 	'titre_synthese_popup_rc' => 'Summary of results',
 	'testable' => "In vitro testable",
-	'titre_organisme_source' => 'Source organism:',
+	'titre_organisme_source' => 'Source material:',
 	'titre_famille_taxo' => 'Taxonomic family:',
 	'titre_ce_groupe_contient_des_produits' => 'This generic group contains the products',
 	'titre_famille_taxo_contient_produits' => 'Examples of products derived from organisms belonging to this family',
 	'titre_famille_mol_contient_allergenes' => 'This molecular family contains the following allergens',
 	'titre_allergenes_testables' => 'In vitro testable allergens',
-	'titre_allergenes_contenus' => 'Allergens in this product',
+	'titre_allergenes_contenus' => 'Allergens contained in this product',
 	'titre_onglet_etudes_produits_allergenes' => 'Products - Allergens Studies',
 	'titre_onglet_etudes_allergenes_allergenes' => 'Allergens - Allergens Studies',
 	'titre_onglet_etudes_produits_produits' => 'Products - Products Studies',
-	'titre_detail_reactivites' => 'Detail of Cross-Reactivity',
-	'titre_reactivites' => 'Cross-Reactivity',
-	'titre_detail_biblio' => 'Details of bibliography data',
+	'titre_detail_reactivites' => 'Detail of Cross-Reactivities',
+	'titre_reactivites' => 'Cross-Reactivities',
+	'titre_detail_biblio' => 'Details of bibliographic data',
 
 
 	// W
-	'work_liste_items_interrogeables' => 'The list of searchable items',
+	'work_liste_items_interrogeables' => 'The list of questionnable items',
 );
 
 
